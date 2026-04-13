5bcab9b34ab102f25d095332d46ab0eb54b19e6e
thiszek9-wq <thiszek9@gmail.com>
2026-04-13 12:28:28 -0700

    Add files via upload

---
+++ Index.tsx
@@ -0,0 +1,27 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import FeaturesSection from "@/components/FeaturesSection";
+import PricingSection from "@/components/PricingSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import FAQSection from "@/components/FAQSection";
+import CTASection from "@/components/CTASection";
+import Footer from "@/components/Footer";
+import ChatBot from "@/components/ChatBot";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background">
+      <Navbar />
+      <HeroSection />
+      <FeaturesSection />
+      <PricingSection />
+      <TestimonialsSection />
+      <FAQSection />
+      <CTASection />
+      <Footer />
+      <ChatBot />
+    </div>
+  );
+};
+
+export default Index;
